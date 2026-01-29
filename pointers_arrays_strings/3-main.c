2b292a60c82d7ab76521003716a72abcaa6d8ea9
Rebaone Seilane <rebaoneseilane@gmail.com>
2026-01-29 09:44:14 +0000

    Task 3

---
+++ pointers_arrays_strings/3-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	_puts(str);
+	
+	return (0);
+}
