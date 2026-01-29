1f7bdf0aa63e185b282abd4df20f7be5d076d2f4
Rebaone Seilane <rebaoneseilane@gmail.com>
2026-01-29 14:11:49 +0000

    Task 10.1

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -4,30 +4,30 @@
  * _atoi - converts a string to an integer
  * @s: pointer to the string
  *
- * Return: the integer converted from the string
+ * Return: integer converted from string
  */
 int _atoi(char *s)
 {
-	int i;
-	int sign;
-	int result;
+	int i, sign, result, found_digit;
 
 	i = 0;
 	sign = 1;
 	result = 0;
+	found_digit = 0;
 
-	/* Handle all signs before the number */
-	while (s[i] != '\0' && (s[i] == ' ' || s[i] == '+' || s[i] == '-'))
+	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
-			sign = -sign;
-		i++;
-	}
+			sign *= -1;
 
-	/* Convert digits to integer */
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		result = result * 10 + (s[i] - '0');
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			found_digit = 1;
+			result = result * 10 + (s[i] - '0');
+		}
+		else if (found_digit)
+			break;
+
 		i++;
 	}
 
