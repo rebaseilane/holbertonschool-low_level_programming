cda883077840ee3ffaa5b52da322e052d6fa35d9
Rebaone Seilane <rebaoneseilane@gmail.com>
2026-01-30 07:00:02 +0000

    Task 5

--- pointers_arrays_strings/5-main.c
+++ pointers_arrays_strings/5-main.c
@@ -1,12 +1,18 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	char s[10] = "My School";
+    char str[] = "Look up!\n";
+    char *ptr;
 
-	printf("%s\n", s);
-	rev_string(s);
-	printf("%s\n", s);
-	return (0);
+    ptr = string_toupper(str);
+    printf("%s", ptr);
+    printf("%s", str);
+    return (0);
 }
