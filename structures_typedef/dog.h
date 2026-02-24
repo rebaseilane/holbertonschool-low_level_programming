976f4e7a1d64f792d0e24e43babcfff96a07fa49
Rebaone Seilane <rebaseilane@gmail.com>
2026-02-24 08:53:48 +0000

    Task 1

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,8 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stddef.h>
+
 /**
  * struct dog - defines a dog
  * @name: name of the dog
@@ -15,5 +17,7 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif /* DOG_H */
