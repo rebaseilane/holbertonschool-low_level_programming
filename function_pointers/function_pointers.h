b8ddf3bfa6f9a0e4a75391743f88f2ea657673f3
Rebaone Seilane <rebaseilane@gmail.com>
2026-02-24 09:55:35 +0000

    Task 2

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,8 +1,11 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
+#include <stddef.h> /* for size_t and NULL */
+
 /* Function prototype for print_name */
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif /* FUNCTION_POINTERS_H */
