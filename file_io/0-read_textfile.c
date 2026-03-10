b00c5b596d1ce7e6c6b8655c0b214f1321d90223
Rebaone Seilane <rebaoneseilane@gmail.com>
2026-03-10 11:19:51 +0000

    Inital coimmit

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,49 +1,52 @@
 #include "main.h"
 #include <fcntl.h>
+#include <unistd.h>
 #include <stdlib.h>
 
 /**
- * read_textfile - Reads a text file and prints it to STDOUT.
+ * read_textfile - Reads a text file and prints it to the POSIX standard output.
  * @filename: Name of the file to read.
  * @letters: Number of letters to read and print.
  *
  * Return: The actual number of letters read and printed.
- *         0 if file cannot be opened, read, or write fails.
+ *         0 if the file cannot be opened or read,
+ *         0 if filename is NULL,
+ *         0 if write fails or does not write the expected amount of bytes.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    ssize_t r_bytes, w_bytes;
-    char *buffer;
+	int fd;
+	ssize_t r_bytes, w_bytes;
+	char *buffer;
 
-    if (!filename)
-        return (0);
+	if (filename == NULL)
+		return (0);
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
-    buffer = malloc(sizeof(char) * letters);
-    if (!buffer)
-    {
-        close(fd);
-        return (0);
-    }
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
 
-    r_bytes = read(fd, buffer, letters);
-    if (r_bytes == -1)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
+	r_bytes = read(fd, buffer, letters);
+	if (r_bytes == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
-    w_bytes = write(STDOUT_FILENO, buffer, r_bytes);
-    free(buffer);
-    close(fd);
+	w_bytes = write(STDOUT_FILENO, buffer, r_bytes);
+	free(buffer);
+	close(fd);
 
-    if (w_bytes != r_bytes)
-        return (0);
+	if (w_bytes != r_bytes)
+		return (0);
 
-    return (w_bytes);
+	return (w_bytes);
 }
--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -5,27 +5,39 @@
 #include <unistd.h>
 
 /**
- * error_exit - Prints an error message and exits with a code
- * @code: Exit code.
- * @msg: Message to print.
- * @arg: Argument to include in the message.
+ * error_exit_str - Prints an error message with a string argument and exits
+ * @code: Exit code
+ * @msg: Error message format
+ * @arg: String argument to include
  */
-void error_exit(int code, char *msg, char *arg)
+void error_exit_str(int code, char *msg, char *arg)
 {
 	dprintf(STDERR_FILENO, msg, arg);
 	exit(code);
 }
 
 /**
- * main - Copies the content of a file to another file.
- * @ac: Number of arguments.
- * @av: Array of arguments.
+ * error_exit_int - Prints an error message with an integer argument and exits
+ * @code: Exit code
+ * @msg: Error message format
+ * @arg: Integer argument to include
+ */
+void error_exit_int(int code, char *msg, int arg)
+{
+	dprintf(STDERR_FILENO, msg, arg);
+	exit(code);
+}
+
+/**
+ * main - Copies the content of a file to another file
+ * @ac: Number of arguments
+ * @av: Array of arguments
  *
- * Return: 0 on success.
+ * Return: 0 on success
  *
  * Description: Usage: cp file_from file_to
- *              Reads 1024 bytes at a time.
- *              Handles errors on open, read, write, and close.
+ *              Reads 1024 bytes at a time
+ *              Handles errors on open, read, write, and close
  */
 int main(int ac, char **av)
 {
@@ -40,13 +52,13 @@ int main(int ac, char **av)
 
 	fd_from = open(av[1], O_RDONLY);
 	if (fd_from == -1)
-		error_exit(98, "Error: Can't read from file %s\n", av[1]);
+		error_exit_str(98, "Error: Can't read from file %s\n", av[1]);
 
 	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
 		close(fd_from);
-		error_exit(99, "Error: Can't write to %s\n", av[2]);
+		error_exit_str(99, "Error: Can't write to %s\n", av[2]);
 	}
 
 	while ((r_bytes = read(fd_from, buffer, 1024)) > 0)
@@ -56,7 +68,7 @@ int main(int ac, char **av)
 		{
 			close(fd_from);
 			close(fd_to);
-			error_exit(99, "Error: Can't write to %s\n", av[2]);
+			error_exit_str(99, "Error: Can't write to %s\n", av[2]);
 		}
 	}
 
@@ -64,13 +76,13 @@ int main(int ac, char **av)
 	{
 		close(fd_from);
 		close(fd_to);
-		error_exit(98, "Error: Can't read from file %s\n", av[1]);
+		error_exit_str(98, "Error: Can't read from file %s\n", av[1]);
 	}
 
 	if (close(fd_from) == -1)
-		error_exit(100, "Error: Can't close fd %d\n", fd_from);
+		error_exit_int(100, "Error: Can't close fd %d\n", fd_from);
 	if (close(fd_to) == -1)
-		error_exit(100, "Error: Can't close fd %d\n", fd_to);
+		error_exit_int(100, "Error: Can't close fd %d\n", fd_to);
 
 	return (0);
 }
